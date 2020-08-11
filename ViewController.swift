19c175f0c6481b53ed0b19b49c40a589839ce485
pramanshugoel <pramanshugoel@gmail.com>
2020-08-11 14:21:25 +0530

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosPOC
+//
+//  Created by Pramanshu Goel on 11/08/20.
+//  Copyright © 2020 Pramanshu Goel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
